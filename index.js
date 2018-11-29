f68d90cc1b6a854bd3440ab57227b19ec2b62b76
Maxim Voronin <maxkvoronin@gmail.com>
2018-11-29 20:21:33 +0300

    simple

---
+++ index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputText = document.getElementById('inputText');
+    const resultText = document.getElementById('resultText');
+    const selectedLang = document.getElementById('selectedLang');
+
+    // выковыриваем из куки значение дефолтового языка
+    const cookieLangValue = document.cookie.replace(/(?:(?:^|.*;\s*)lang\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+    //если нет такого значения, то не ставим его списку
+    if (cookieLangValue) {
+        selectedLang.value = cookieLangValue;
+    }
+
+    //после изменении языка в списке очищаем инпут и ставим (меняем) значение lang в куку
+    selectedLang.addEventListener("change", () => {
+        inputText.value = "";
+        document.cookie = `lang=${selectedLang.value}`;
+    });
+
+    //после изменения input фетчим запрос на яндекс-транслейт и выводим результат
+    inputText.addEventListener("change", async () => {
+        const url = new URL('https://translate.yandex.net/api/v1.5/tr.json/translate');
+        url.search = new URLSearchParams({
+            key: "trnsl.1.1.20181129T142159Z.6244355c0c2a243e.61a7a1248ef538f61bc4c3617d4c03475a704eb2",
+            text: inputText.value,
+            lang: selectedLang.value,
+        });
+
+        const res = await fetch(url);
+        const json = await res.json();
+        resultText.innerText = json.text[0];
+    });
+});
